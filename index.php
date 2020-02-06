078a556dc791d202c0192987babdf9cec5cb3069
sm <sajid@7gmedia.com>
2020-02-06 17:20:29 +0400

    align items center
    Signed-off-by: sm <sajid@7gmedia.com>

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/style.css">    
+</head>
+<body>
+	<div class="main_wrapper">
+		<div class="align_center">
+			<h1>Align Content Vertically Center</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam eum ipsum aspernatur facere aliquid eius vitae ipsam rerum porro debitis facilis voluptate blanditiis, consequuntur recusandae optio assumenda iste sequi magni!</p>
+		</div>
+	</div>
+</body>
+</html>
